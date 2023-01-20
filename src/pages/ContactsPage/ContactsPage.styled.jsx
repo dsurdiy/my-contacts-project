65bee7126f13d6e3221142435de4b2a3843c209c
DimaSurdiy <dsurdiy@ukr.net>
2023-01-20 06:57:28 +0200

    styles for contacts page added

---
+++ src/pages/ContactsPage/ContactsPage.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const PageTitle = styled.h2`
+  margin-top: 30px;
+  text-align: center;
+  font-size: 35px;
+`;
+
+export const ContactsContainer = styled.div`
+  width: 700px;
+`;
